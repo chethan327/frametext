fb4796c3cb23390325058f9c2c3651b1ea85c01b
Chethan H <chethanh327@gmail.com>
2026-06-14 18:47:41 +0530

    Delete background.js

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'open_url') {
-    chrome.tabs.create({ url: request.url });
-    sendResponse({ success: true });
-  }
-});
